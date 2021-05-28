9dfcfc58e9d8f2d3cc5a47bb37380d0ac8a2ecf3
celinediaz <38956985+celinediaz@users.noreply.github.com>
2021-05-28 15:49:51 -0500

    Tic tac toe
    tic tac toe using function factories and originally I was using the module patten but when adding the event listeners I called that function rather than it being an IIFE. I might edit it eventually. The design is also pretty basic but uh yeah. I guess I will clean it up and use the module pattern later.

---
+++ script.js
@@ -0,0 +1,136 @@
+const boardcontainer = document.querySelector('.board');
+const winnermessage = document.querySelector('.winner');
+const startmessage = document.querySelector('.start');
+const winner =  document.querySelector('.won');
+let Player = (mark, name, id) => {
+    return {
+        mark,
+        name,
+        id
+    }
+}
+
+const gameboard = ((player1, player2) => {
+    const boardcontainer = document.querySelector('.board');
+    let board = [...Array(3)].map(x => Array(3).fill(""));
+    for(let i=0; i < 3; i++){
+        for(let j=0; j< 3; j++){
+            const item = document.createElement('div');
+            item.dataset.row = i;
+            item.dataset.column = j;
+            item.classList.add('item');
+            item.addEventListener("click", function() {
+                placeMark(+this.dataset.row, +this.dataset.column);
+            });
+            boardcontainer.appendChild(item);
+        }
+    }
+    let turn = 0;
+    currentPlayer = player1;
+
+    const togglePlayer = () => {
+        currentPlayer = currentPlayer.id ? player2 : player1;
+    }
+
+    const placeMark = (row, col) => {
+        if(board[row][col] === "") { 
+        board[row][col] = currentPlayer.mark;
+        const item = document.querySelector(`[data-row='${row}'][data-column='${col}']`);
+        if(currentPlayer.mark === "o") {
+            const circle = document.createElement('div');
+            circle.classList.add('circle');
+            item.appendChild(circle);
+        }else{
+            const times = document.createElement('i');
+            times.classList.add('fas', 'fa-times', 'fa-8x')
+            item.appendChild(times);
+        }
+        checkWinner(row, col, currentPlayer);
+        if(player2){
+            togglePlayer();
+        }else {
+            botPlacesMark();
+        }
+        turn++;
+    }
+    }
+    const botPlacesMark =() => {
+            let availableCoords  = [];
+            const bot = Player("o", "Bot", false);
+            board.forEach((row, rowNum) => row.forEach((mark, colNum) => {
+                if(mark === "") { availableCoords .push({rowNum, colNum}) }
+            }));
+            let randomCoord = Math.floor(Math.random() * availableCoords.length);
+            let row = availableCoords[randomCoord].rowNum;
+            let col = availableCoords[randomCoord].colNum;
+            board[row][col] = "o";
+            const item = document.querySelector(`[data-row='${row}'][data-column='${col}']`);
+            const circle = document.createElement('div');
+            circle.classList.add('circle');
+            setTimeout(function(){ item.appendChild(circle);
+            checkWinner(row, col, bot);
+            turn++;
+            }, 200);
+    }
+
+    const checkWinner = (row, col, currentPlayer) =>{
+        if (turn >= 4 && (checkHorizontal(row, col, currentPlayer) || checkVertical(row, col, currentPlayer) || checkDiag1(row, col, currentPlayer) || checkDiag2(row, col, currentPlayer))) {
+            winnermessage.classList.add('flex');
+            winner.textContent = `${currentPlayer.name} won`;
+        }else if(turn === 8){
+            winnermessage.classList.add('flex');
+            winner.textContent = `Nobody won lol`;
+        }
+    }
+    const checkHorizontal = (row, col, currentPlayer) => {
+        let rep = 1;
+        for (let i = col; i < 2 && board[row][i + 1] === currentPlayer.mark; i++, ++rep);
+        for (let i = col; i >= 1 && board[row][i - 1] === currentPlayer.mark; i--, ++rep);
+        return rep >= 3;
+    }
+    const checkVertical = (row, col, currentPlayer) => {
+        let rep = 1;
+        for (let i = row; i < 2 && board[i + 1][col] === currentPlayer.mark; i++, ++rep);
+        for (let i = row; i >= 1 && board[i - 1][col] === currentPlayer.mark; i--, ++rep);
+        return rep >= 3;
+    }
+    const checkDiag1 = (row, col, currentPlayer) => {
+        let rep = 1;
+        for (let i = row, j = col; i < 2 && j < 2 && board[i + 1][j + 1] === currentPlayer.mark; i++, j++, rep++);
+        for (let i = row, j = col; i >= 1 && j >= 1 && board[i - 1][j - 1] === currentPlayer.mark; i--, j--, rep++);
+        return rep >= 3;
+    }
+    const checkDiag2 = (row, col, currentPlayer) => {
+        let rep = 1;
+        for (let i = row, j = col; i >= 1 && j < 2 && board[i - 1][j + 1] === currentPlayer.mark; i--, j++, rep++);
+        for (let i = row, j = col; i < 2 && j >= 1 && board[i + 1][j - 1] === currentPlayer.mark; i++, j--, rep++);
+        return rep >= 3;
+    }
+
+    return {
+        board
+    };
+});
+
+const eventListeners = (() => {
+    document.getElementById("submit").addEventListener("click", function (e){
+        e.preventDefault();
+        const p1 = document.getElementById("p1").value;
+        const p2 = document.getElementById("p2").value;
+        const player1 = Player("x", p1, true);
+        const player2 = Player("o", p2, false);
+        startmessage.classList.add('none');
+        startmessage.classList.remove('flex');
+        gameboard(player1, player2);
+    });
+    document.getElementById("AI").addEventListener("click", function (e){
+        e.preventDefault();
+        const p1 = document.getElementById("p1").value;
+        const player1 = Player("x", p1, true);
+        startmessage.classList.add('none');
+        startmessage.classList.remove('flex');
+        gameboard(player1);
+    });
+    document.getElementById("reset").addEventListener("click", () => window.location.reload());
+})();
+
